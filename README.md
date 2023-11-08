2d5f409f743e86b2f1baeb88d891774c8b6d582b
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-11-08 16:22:05 +0000

    DOC-9: Added docker compose local containers and bash script

--- README.md
+++ README.md
@@ -5,3 +5,14 @@
 [![API docs](https://img.shields.io/badge/API_docs_-view-85EA2D.svg?logo=swagger)](https://hmpps-document-management-api-dev.hmpps.service.justice.gov.uk/webjars/swagger-ui/index.html?configUrl=/v3/api-docs)
 
 A shared electronic document management API.
+
+# Running the service locally using run-local.sh
+This will run the service locally. It starts the database and localstack containers then start the service via a bash script.
+It connects to the dev version of hmpps-auth.
+
+Run the following commands from the root directory of the project:
+
+1. docker compose -f docker-compose-local.yml pull
+2. docker compose -f docker-compose-local.yml up --no-start
+3. docker compose -f docker-compose-local.yml start db localstack
+4. ./run-local.sh
---
+++ run-local.sh
@@ -0,0 +1,27 @@
+#
+# This script is used to run the Document Management API locally, to interact with
+# existing PostgreSQL, localstack and hmpps-auth containers.
+#
+# It runs with a combination of properties from the default spring profile (in application.yaml) and supplemented
+# with the -local profile (from application-local.yml). The latter overrides some of the defaults.
+#
+# The environment variables here will also override values supplied in spring profile properties, specifically
+# around removing the SSL connection to the database and setting the DB properties, SERVER_PORT and client credentials
+# to match those used in the docker-compose files.
+#
+
+# Provide the DB connection details to local container-hosted Postgresql DB
+# Match with the credentials set in docker-compose.yml
+export DB_SERVER=localhost
+export DB_NAME=document-management-db
+export DB_USER=document-management
+export DB_PASS=document-management
+export DB_SSL_MODE=prefer
+
+# Provide URLs to other dependent services. Dev services used here (can be local if you set uo the dependent services locally)
+export HMPPS_AUTH_URL=https://sign-in-dev.hmpps.service.justice.gov.uk/auth
+
+# Run the application with stdout and local profiles active
+SPRING_PROFILES_ACTIVE=stdout,local ./gradlew bootRun
+
+# End
