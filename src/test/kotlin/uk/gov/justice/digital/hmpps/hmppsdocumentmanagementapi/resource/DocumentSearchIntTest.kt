76dacdf4c9a580def7e7230ea26afd2ab96aa3d1
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-11-17 15:05:54 +0000

    DOC-25: Integration tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsdocumentmanagementapi/resource/DocumentSearchIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsdocumentmanagementapi/resource/DocumentSearchIntTest.kt
@@ -11,19 +11,118 @@ import uk.gov.justice.digital.hmpps.hmppsdocumentmanagementapi.enumeration.Docum
 import uk.gov.justice.digital.hmpps.hmppsdocumentmanagementapi.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.hmppsdocumentmanagementapi.model.DocumentSearchRequest
 import uk.gov.justice.digital.hmpps.hmppsdocumentmanagementapi.model.DocumentSearchResult
+import java.util.UUID
 
 class DocumentSearchIntTest : IntegrationTestBase() {
+  private val deletedDocumentUuid = UUID.fromString("f73a0f91-2957-4224-b477-714370c04d37")
+
   @Sql("classpath:test_data/document-search.sql")
   @Test
   fun `response contains search request`() {
     val documentType = DocumentType.HMCTS_WARRANT
-    val metadata = JacksonUtil.toJsonNode(" {\"prisonNumber\": \"A1234BC\" }")
+    val metadata = JacksonUtil.toJsonNode("{ \"prisonNumber\": \"A1234BC\" }")
 
     val response = webTestClient.searchDocuments(documentType, metadata)!!
 
     with(response.request) {
-      assertThat(documentType).isEqualTo(documentType)
-      assertThat(metadata).isEqualTo(metadata)
+      assertThat(this.documentType).isEqualTo(documentType)
+      assertThat(this.metadata).isEqualTo(metadata)
+    }
+  }
+
+  @Sql("classpath:test_data/document-search.sql")
+  @Test
+  fun `search warrants by prison number`() {
+    val documentType = DocumentType.HMCTS_WARRANT
+    val metadata = JacksonUtil.toJsonNode("{ \"prisonNumber\": \"A1234BC\" }")
+
+    val response = webTestClient.searchDocuments(documentType, metadata)!!
+
+    response.results.onEach {
+      assertThat(it.documentType).isEqualTo(documentType)
+      assertThat(it.metadata["prisonNumber"].asText()).isEqualTo("A1234BC")
+    }
+  }
+
+  @Sql("classpath:test_data/document-search.sql")
+  @Test
+  fun `search all document types by prison number`() {
+    val documentType = null
+    val metadata = JacksonUtil.toJsonNode("{ \"prisonNumber\": \"A1234BC\" }")
+
+    val response = webTestClient.searchDocuments(documentType, metadata)!!
+
+    with(response.results) {
+      assertThat(map { it.documentType }).isEqualTo(listOf(DocumentType.HMCTS_WARRANT, DocumentType.SUBJECT_ACCESS_REQUEST_REPORT))
+      onEach {
+        assertThat(it.metadata["prisonNumber"].asText()).isEqualTo("A1234BC")
+      }
+    }
+  }
+
+  @Sql("classpath:test_data/document-search.sql")
+  @Test
+  fun `search metadata is case insensitive`() {
+    val documentType = DocumentType.HMCTS_WARRANT
+    val metadata = JacksonUtil.toJsonNode("{ \"court\": \"stafford crown\" }")
+
+    val response = webTestClient.searchDocuments(documentType, metadata)!!
+
+    response.results.onEach {
+      assertThat(it.metadata["court"].asText()).isEqualTo("Stafford Crown")
+    }
+  }
+
+  @Sql("classpath:test_data/document-search.sql")
+  @Test
+  fun `search metadata contains text`() {
+    val documentType = DocumentType.HMCTS_WARRANT
+    val metadata = JacksonUtil.toJsonNode("{ \"court\": \"agist\" }")
+
+    val response = webTestClient.searchDocuments(documentType, metadata)!!
+
+    response.results.onEach {
+      assertThat(it.metadata["court"].asText()).contains("Magistrates")
+    }
+  }
+
+  @Sql("classpath:test_data/document-search.sql")
+  @Test
+  fun `search string array metadata property`() {
+    val documentType = DocumentType.HMCTS_WARRANT
+    val metadata = JacksonUtil.toJsonNode("{ \"previousPrisonNumbers\": \"A1234BC\" }")
+
+    val response = webTestClient.searchDocuments(documentType, metadata)!!
+
+    response.results.onEach { document ->
+      assertThat(document.metadata["previousPrisonNumbers"].map { it.asText() }).contains("A1234BC")
+    }
+  }
+
+  @Sql("classpath:test_data/document-search.sql")
+  @Test
+  fun `search by multiple metadata properties`() {
+    val documentType = DocumentType.HMCTS_WARRANT
+    val metadata = JacksonUtil.toJsonNode("{ \"prisonCode\": \"SFI\", \"prisonNumber\": \"D4567EF\" }")
+
+    val response = webTestClient.searchDocuments(documentType, metadata)!!
+
+    response.results.onEach {
+      assertThat(it.metadata["prisonCode"].asText()).isEqualTo("SFI")
+      assertThat(it.metadata["prisonNumber"].asText()).isEqualTo("D4567EF")
+    }
+  }
+
+  @Sql("classpath:test_data/document-search.sql")
+  @Test
+  fun `search does not return deleted documents`() {
+    val documentType = DocumentType.HMCTS_WARRANT
+    val metadata = JacksonUtil.toJsonNode("{ \"prisonNumber\": \"A1234BC\" }")
+
+    val response = webTestClient.searchDocuments(documentType, metadata)!!
+
+    response.results.onEach {
+      assertThat(it.documentUuid).isNotEqualTo(deletedDocumentUuid)
     }
   }
 
