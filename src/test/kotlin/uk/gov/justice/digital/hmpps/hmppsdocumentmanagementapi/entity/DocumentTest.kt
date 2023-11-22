a3a613e20d9e6b561ae65605cd383e80b0e1aa06
David Winchurch <david.winchurch@digital.justice.gov.uk>
2023-11-22 09:53:30 +0000

    DOC-28: Document service unit tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsdocumentmanagementapi/entity/DocumentTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsdocumentmanagementapi/entity/DocumentTest.kt
@@ -2,15 +2,30 @@ package uk.gov.justice.digital.hmpps.hmppsdocumentmanagementapi.entity
 
 import io.hypersistence.utils.hibernate.type.json.internal.JacksonUtil
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.within
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.hmppsdocumentmanagementapi.enumeration.DocumentType
 import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 import uk.gov.justice.digital.hmpps.hmppsdocumentmanagementapi.model.Document as DocumentModel
 
 class DocumentTest {
+  private val document =
+    Document(
+      documentId = 1,
+      documentType = DocumentType.HMCTS_WARRANT,
+      filename = "warrant_for_sentencing",
+      fileExtension = "pdf",
+      fileSize = 3876,
+      fileHash = "d58e3582afa99040e27b92b13c8f2280",
+      mimeType = "application/pdf",
+      metadata = JacksonUtil.toJsonNode("{ \"prisonCode\": \"KMI\", \"prisonNumber\": \"A1234BC\" }"),
+      createdByServiceName = "Remand and sentencing",
+      createdByUsername = "CREATED_BY_USERNAME",
+    )
+
   @Test
   fun `document contains replaced metadata`() {
-    val document = document()
     val replacementMetadata = JacksonUtil.toJsonNode("{ \"prisonCode\": \"RSI\", \"prisonNumber\": \"B2345CD\" }")
 
     document.replaceMetadata(replacementMetadata, supersededByServiceName = "Replaced metadata using service name", supersededByUsername = "REPLACED_BY_USERNAME")
@@ -20,7 +35,6 @@ class DocumentTest {
 
   @Test
   fun `metadata history contains original metadata`() {
-    val document = document()
     val originalMetadata = document.metadata
     val replacementMetadata = JacksonUtil.toJsonNode("{ \"prisonCode\": \"RSI\", \"prisonNumber\": \"B2345CD\" }")
 
@@ -31,7 +45,6 @@ class DocumentTest {
 
   @Test
   fun `metadata history contains audit information`() {
-    val document = document()
     val replacementMetadata = JacksonUtil.toJsonNode("{ \"prisonCode\": \"RSI\", \"prisonNumber\": \"B2345CD\" }")
     val now = LocalDateTime.now()
     val serviceName = "Replaced metadata using service name"
@@ -49,9 +62,16 @@ class DocumentTest {
   }
 
   @Test
-  fun `to model`() {
-    val document = document()
+  fun `metadata history uses now as the default`() {
+    val replacementMetadata = JacksonUtil.toJsonNode("{ \"prisonCode\": \"RSI\", \"prisonNumber\": \"B2345CD\" }")
 
+    document.replaceMetadata(replacementMetadata, supersededByServiceName = "Replaced metadata using service name", supersededByUsername = "REPLACED_BY_USERNAME")
+
+    assertThat(document.documentMetadataHistory().single().supersededTime).isCloseTo(LocalDateTime.now(), within(3, ChronoUnit.SECONDS))
+  }
+
+  @Test
+  fun `to model`() {
     assertThat(document.toModel()).isEqualTo(
       with(document) {
         DocumentModel(
@@ -73,8 +93,6 @@ class DocumentTest {
 
   @Test
   fun `to models`() {
-    val document = document()
-
     assertThat(listOf(document).toModels()).isEqualTo(
       listOf(
         with(document) {
@@ -95,18 +113,4 @@ class DocumentTest {
       ),
     )
   }
-
-  private fun document() =
-    Document(
-      documentId = 1,
-      documentType = DocumentType.HMCTS_WARRANT,
-      filename = "warrant_for_sentencing",
-      fileExtension = "pdf",
-      fileSize = 3876,
-      fileHash = "d58e3582afa99040e27b92b13c8f2280",
-      mimeType = "application/pdf",
-      metadata = JacksonUtil.toJsonNode("{ \"prisonCode\": \"KMI\", \"prisonNumber\": \"A1234BC\" }"),
-      createdByServiceName = "Remand and sentencing",
-      createdByUsername = "CREATED_BY_USERNAME",
-    )
 }
