c5fa50bf49e70eb9a17c3fc92d5c58ceb1b72082
Jalal ul Deen <jalal.uldeen@digital.justice.gov.uk>
2024-01-08 18:17:57 +0000

    CVSL-1568 removed HMPPS
    removed the HMPPS from suffix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsdocumentmanagementapi/enumeration/DocumentType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsdocumentmanagementapi/enumeration/DocumentType.kt
@@ -13,7 +13,7 @@ enum class DocumentType(
     description = "Subject Access Request Report",
     additionalRoles = setOf(ROLE_DOCUMENT_TYPE_SAR),
   ),
-  HHMPPS_CVL_DOCS(
+  CVL_DOCS(
     description = "Offender Licence documents created and managed by CVL (create and vary a licence create-and-vary-a-licence-api)",
   ),
 }
