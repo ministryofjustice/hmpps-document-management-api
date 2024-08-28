a7f77cdfb1e9d23dadc815b69e3dbedddb3653d7
Faiz Ibrahim <faiz.ibrahim@digital.justice.gov.uk>
2024-08-28 11:14:12 +0100

    test vul fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.4"
-  kotlin("plugin.spring") version "1.9.23"
-  kotlin("plugin.jpa") version "1.9.23"
+  kotlin("plugin.spring") version "2.0.20"
+  kotlin("plugin.jpa") version "2.0.20"
   jacoco
 }
 
