8242f5e34b110e68547e6006552c5fcb244cd8d5
Md Hasan Rahman <Hassan.rahman@justice.gov.uk>
2025-06-05 19:20:16 +0100

    RASS-1047-adding document types for sentence and remand doc upload to db

---
+++ src/main/resources/migration/common/V12__ras_upload_document_type.sql
@@ -0,0 +1,4 @@
+INSERT INTO document_type VALUES ('TRIAL_RECORD_SHEET', 'Trial record sheet document.'),
+                                 ('INDICTMENT', 'Indictment document.'),
+                                 ('PRISON_COURT_REGISTER', 'Prison court register document.');
+
