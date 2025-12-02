67e4239dd907949f315ea8ba279e034df91b8245
Carlo Veo <carlo.veo2@justice.gov.uk>
2025-12-02 07:51:17 +0000

    RASS-1675 add non sentencing document types

---
+++ src/main/resources/migration/common/V13__add_non_sentencing_document_types.sql
@@ -0,0 +1,5 @@
+INSERT INTO document_type VALUES ('BAIL_ORDER', 'Bail order'),
+                                 ('SUSPENDED_IMPRISONMENT_ORDER', 'Suspended imprisonment order'),
+                                 ('NOTICE_OF_DISCONTINUANCE', 'Notice of discontinuance'),
+                                 ('COMMUNITY_ORDER', 'Community order');
+
