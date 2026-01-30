d91ec17989ab2cea260c363b701a3e5ac5a26c4b
Md Hasan Rahman <Hassan.rahman@justice.gov.uk>
2026-01-30 12:01:17 +0000

    RASS-1844-formatted

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsdocumentmanagementapi/config/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsdocumentmanagementapi/config/ResourceServerConfiguration.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.hmppsdocumentmanagementapi.config
 
-import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
